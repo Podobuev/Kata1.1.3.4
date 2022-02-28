6b406f9fffa9c75a48861b5f8f6455b5a3f18417
Андрей Подобуев <podobuev@MacBook-Pro-Andrej.local>
2022-02-28 22:25:28 +0300

    v3

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private UserDao userDao = new UserDaoJDBCImpl();
+    private UserDao userDao = new UserDaoHibernateImpl();
 
     @Override
     public void createUsersTable() {
